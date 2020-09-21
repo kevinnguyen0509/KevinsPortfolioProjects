ae7743b8058aeea7a4acd3376340ddc2b264abb0
Kevin Nguyen <Kevin.Nguyen0509@gmail.com>
2020-09-21 15:55:59 -0700

    Animated front
    Added script to animate button and description on home page

---
+++ kevinsPortfolio/js/index.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function(){
+    $("#description").hide();
+    $("#button").hide();
+    
+    $("#description").slideDown(1000, function(){
+        //Auto slides
+    });
+
+    $("#button").slideDown(400, function(){
+        //Auto slides
+    });
+    
+});
