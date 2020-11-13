5d758081c4e309d1908866bcf7c383c5e26064d1
Kevin Nguyen <Kevin.Nguyen0509@gmail.com>
2020-11-13 15:17:02 -0800

    fixed nav bar
    created js file to follow rules of DRY

---
+++ kevinsPortfolio/Projects/showcase/Best City Guide/js/getNavbar.js
@@ -0,0 +1,16 @@
+$( document ).ready(function() {
+    var nav = `<h1 class="name"><a href="index.html">Best City Guide</a></h1>
+		<ul class="main-nav">
+			<li><a href="icecream.html">ice cream</a></li>
+			<li><a href="donuts.html">donuts</a></li>
+			<li><a href="tea.html">tea</a></li>
+			<li><a href="coffee.html">coffee</a></li>
+			<li><a href="learnMore.html">Learn More!</a></li>
+		</ul>`
+    document.getElementById("main-header").innerHTML = nav;
+});    
+
+
+
+
+
