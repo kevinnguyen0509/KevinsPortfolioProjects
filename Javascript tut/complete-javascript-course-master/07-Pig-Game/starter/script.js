819952b0df573425f5212e810cb4949caaad5c69
Kevin Nguyen <Kevin.Nguyen0509@gmail.com>
2021-04-10 22:37:28 -0700

    working on player 1
    -add ability to click roll dice.
    -added a hidden class in css to hide dice
    score calculates up when roll dice pressed.
    -if 1 is rolled currentRollScore goes to 0

--- Javascript tut/complete-javascript-course-master/07-Pig-Game/starter/script.js
+++ Javascript tut/complete-javascript-course-master/07-Pig-Game/starter/script.js
@@ -1,1 +1,34 @@
 'use strict';
+
+// Selecting elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.querySelector('#score--1');
+const currentRollScorePlayer0 = document.querySelector('#current--0');
+const currentRollScorePlayer1 = document.querySelector('#current--1');
+const dice = document.querySelector('.dice');
+const newGameButton = document.querySelector('.btn--new');
+const rollDiceButton = document.querySelector('.btn--roll');
+const holdButtong = document.querySelector('.btn--hold');
+let currentRollScore = 0;
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+dice.classList.add('hidden');
+
+rollDiceButton.addEventListener('click', function () {
+  //1) Generate a random dice roll
+  const diceRollResult = Math.trunc(Math.random() * 6) + 1;
+  //2) Display dice
+  dice.classList.remove('hidden');
+  dice.src = `img/dice-${diceRollResult}.png`;
+  //3) check for rolled 1: if true, switch to next player
+  if (diceRollResult !== 1) {
+    currentRollScore += diceRollResult;
+    currentRollScorePlayer0.textContent = currentRollScore;
+  } else {
+    //current rollScore = 0
+    currentRollScore = 0;
+    currentRollScorePlayer0.textContent = currentRollScore;
+    //switch players
+  }
+});
