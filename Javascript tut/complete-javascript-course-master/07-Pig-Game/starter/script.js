1774ec72a288f782b8370d237e0d34b876161c72
Kevin Nguyen <Kevin.Nguyen0509@gmail.com>
2021-04-11 16:43:00 -0700

    add switching feature
    Game switches if you get a 1

--- Javascript tut/complete-javascript-course-master/07-Pig-Game/starter/script.js
+++ Javascript tut/complete-javascript-course-master/07-Pig-Game/starter/script.js
@@ -1,6 +1,9 @@
 'use strict';
 
 // Selecting elements
+const player0Element = document.querySelector('.player--0');
+const player1Element = document.querySelector('.player--1');
+
 const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
 const currentRollScorePlayer0 = document.querySelector('#current--0');
@@ -9,11 +12,14 @@ const dice = document.querySelector('.dice');
 const newGameButton = document.querySelector('.btn--new');
 const rollDiceButton = document.querySelector('.btn--roll');
 const holdButtong = document.querySelector('.btn--hold');
-let currentRollScore = 0;
 
 score0El.textContent = 0;
 score1El.textContent = 0;
 dice.classList.add('hidden');
+
+const score = [0, 0];
+let currentRollScore = 0;
+let activePlayer = 0;
 
 rollDiceButton.addEventListener('click', function () {
   //1) Generate a random dice roll
@@ -24,11 +30,18 @@ rollDiceButton.addEventListener('click', function () {
   //3) check for rolled 1: if true, switch to next player
   if (diceRollResult !== 1) {
     currentRollScore += diceRollResult;
-    currentRollScorePlayer0.textContent = currentRollScore;
+    document.getElementById(
+      `current--${activePlayer}`
+    ).textContent = currentRollScore;
   } else {
     //current rollScore = 0
     currentRollScore = 0;
-    currentRollScorePlayer0.textContent = currentRollScore;
+    document.getElementById(
+      `current--${activePlayer}`
+    ).textContent = currentRollScore;
     //switch players
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0Element.classList.toggle('player--active');
+    player1Element.classList.toggle('player--active');
   }
 });
