ab7893d672c1971ea6d77a9c89358d37132cf15e
Kevin Nguyen <Kevin.Nguyen0509@gmail.com>
2021-04-11 17:38:57 -0700

    add hold and new game button
    finished app to include new game button and hold button

--- Javascript tut/complete-javascript-course-master/07-Pig-Game/starter/script.js
+++ Javascript tut/complete-javascript-course-master/07-Pig-Game/starter/script.js
@@ -11,7 +11,7 @@ const currentRollScorePlayer1 = document.querySelector('#current--1');
 const dice = document.querySelector('.dice');
 const newGameButton = document.querySelector('.btn--new');
 const rollDiceButton = document.querySelector('.btn--roll');
-const holdButtong = document.querySelector('.btn--hold');
+const holdButton = document.querySelector('.btn--hold');
 
 score0El.textContent = 0;
 score1El.textContent = 0;
@@ -21,6 +21,17 @@ const score = [0, 0];
 let currentRollScore = 0;
 let activePlayer = 0;
 
+function switchPlayers() {
+  //current rollScore = 0
+  currentRollScore = 0;
+  document.getElementById(
+    `current--${activePlayer}`
+  ).textContent = currentRollScore;
+  //switch players
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0Element.classList.toggle('player--active');
+  player1Element.classList.toggle('player--active');
+}
 rollDiceButton.addEventListener('click', function () {
   //1) Generate a random dice roll
   const diceRollResult = Math.trunc(Math.random() * 6) + 1;
@@ -34,14 +45,44 @@ rollDiceButton.addEventListener('click', function () {
       `current--${activePlayer}`
     ).textContent = currentRollScore;
   } else {
-    //current rollScore = 0
-    currentRollScore = 0;
-    document.getElementById(
-      `current--${activePlayer}`
-    ).textContent = currentRollScore;
-    //switch players
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    player0Element.classList.toggle('player--active');
-    player1Element.classList.toggle('player--active');
+    switchPlayers();
   }
+});
+
+holdButton.addEventListener('click', function () {
+  //1) Add current score to active player's score
+  score[activePlayer] += currentRollScore;
+  document.getElementById(`current--${activePlayer}`).textContent =
+    score[activePlayer];
+
+  switchPlayers(); //switch player
+  score0El.textContent = score[0];
+  score1El.textContent = score[1];
+  //2) check if player's score is >= 100 playerscore = playerscore >= 100 ? close game: add score to Total score then do step 3
+
+  if (score[0] >= 100) {
+    player0Element.classList.add('player--winner');
+    rollDiceButton.disabled = true;
+    holdButton.disabled = true;
+  } else if (score[1] >= 100) {
+    console.log('player 2 wins');
+    player1Element.classList.add('player--winner');
+    rollDiceButton.disabled = true;
+    holdButton.disabled = true;
+  }
+});
+
+newGameButton.addEventListener('click', function () {
+  switchPlayers();
+  score[0] = 0;
+  score[1] = 0;
+  score0El.textContent = score[0];
+  score1El.textContent = score[1];
+  currentRollScore = 0;
+
+  player1Element.classList.remove('player--winner');
+  player0Element.classList.remove('player--winner');
+
+  rollDiceButton.disabled = false;
+  holdButton.disabled = false;
 });
