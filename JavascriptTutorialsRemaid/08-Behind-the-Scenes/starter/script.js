8d1ef7ec2f2fc1e3e7f85b3d79706cb6ba0130a5
Kevin Nguyen <Kevin.Nguyen0509@gmail.com>
2021-04-18 14:28:40 -0700

    add 3 sections
    added cities, quotes and contact

--- JavascriptTutorialsRemaid/08-Behind-the-Scenes/starter/script.js
+++ JavascriptTutorialsRemaid/08-Behind-the-Scenes/starter/script.js
@@ -1,9 +1,19 @@
 'use strict';
 
-function calcAge() {
-  isAVar = 'let here.';
-  let isAVar;
-  console.log(isAVar);
-}
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    let targetTemp = target - nums[i];
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[j] == target - nums[i]) {
+        return console.log(i + ' ' + j);
+      }
+    }
+  }
+};
 
-calcAge();
+twoSum([3, 2, 4], 6);
