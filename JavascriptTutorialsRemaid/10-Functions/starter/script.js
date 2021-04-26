3328a99c69c7fe34a2097b9fae4ad6b5167ddba2
Kevin Nguyen <Kevin.Nguyen0509@gmail.com>
2021-04-26 11:02:58 -0700

    function refresher test

--- JavascriptTutorialsRemaid/10-Functions/starter/script.js
+++ JavascriptTutorialsRemaid/10-Functions/starter/script.js
@@ -1,1 +1,35 @@
 'use strict';
+
+// const bookings = [];
+
+// const createBooking = function (flightNum, numPassengers = 1, price = 199) {
+//   const booking = {
+//     flightNum,
+//     numPassengers,
+//     price,
+//   };
+//   console.log(booking);
+//   bookings.push(booking);
+// };
+
+// createBooking('LH123');
+// createBooking('LH123', 2, 800);
+
+const flight = 'LH234';
+const kevin = {
+  name: 'Kevin Nguyen',
+  passport: 321468745132,
+};
+
+const checkin = function (flightNum, passenger) {
+  flightNum = 64546;
+  passenger.name = 'Mr.' + passenger.name;
+
+  if (passenger.passport === 654654) alert('Checked in');
+  else {
+    alert('Wrong passport');
+  }
+};
+checkin(flight, kevin);
+console.log(flight);
+console.log(kevin);
