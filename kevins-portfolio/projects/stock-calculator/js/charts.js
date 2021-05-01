b072d19a5328427e0267941eacd44458fbd306ac
Kevin Nguyen <Kevin.Nguyen0509@gmail.com>
2021-05-01 02:48:06 -0700

    fixed bug on start up screen for charts
    Charts now take in the width of parent container to increase to the correct size instead of only taking 1/4 of the screen.

--- kevins-portfolio/projects/stock-calculator/js/charts.js
+++ kevins-portfolio/projects/stock-calculator/js/charts.js
@@ -1,6 +1,11 @@
 //https://canvasjs.com/docs/charts/basics-of-creating-html5-chart/updating-chart-options/
 
 window.onload = function () {
+  let searchBtn = document.querySelector(".search-container__search-icon");
+  let allCharts = document.querySelectorAll(".chart-container__charts");
+  let titles = document.querySelectorAll(".title-container");
+  let chartContainer = document.querySelectorAll(".chart-container");
+  let loading = document.querySelector(".loading");
   let chartObject = {
     animationEnabled: true,
     theme: "light2", // "light1", "light2", "dark1", "dark2"
@@ -10,6 +15,7 @@ window.onload = function () {
     axisY: {
       title: "Reserves(MMbbl)",
     },
+
     data: [
       {
         type: "column",
@@ -17,7 +23,7 @@ window.onload = function () {
         legendMarkerColor: "grey",
         legendText: "MMbbl = one million barrels",
         dataPoints: [
-          { y: 300878, label: "Venezuela" },
+          { y: 300684, label: "Venezuela" },
           { y: 266455, label: "Saudi" },
           { y: 169709, label: "Canada" },
           { y: 158400, label: "Iran" },
@@ -74,26 +80,65 @@ window.onload = function () {
     "chartContainer11",
     createDeepCopy(chartObject)
   );
-  let chart12 = new CanvasJS.Chart(
-    "chartContainer12",
-    createDeepCopy(chartObject)
-  );
 
-  chart.options.title.text = "Revenue Growth: 10% + is good";
-  chart2.options.title.text = "Chart 222222";
-  chart3.options.title.text = "Chart 3!!";
+  let changeTitle = () => {
+    chart.options.title.text = "Revenue Growth: 10% + is good";
+    chart2.options.title.text = "Net income growth: 12% + is good";
+    chart3.options.title.text = "eps growth: 10% +";
+    chart5.options.title.text = "net income growth: 15% +";
+    chart6.options.title.text = "Cash/Sales ratio: 5% +";
+    chart7.options.title.text = "Return on assets ratio: 7% +";
+    chart8.options.title.text = "Return on Equity ratio: 15% +";
+  };
 
-  chart2.render();
-  chart.render();
-  chart3.render();
+  let renderCharts = () => {
+    chart2.render();
+    chart.render();
+    chart3.render();
 
-  chart5.render();
-  chart6.render();
-  chart7.render();
-  chart8.render();
+    chart5.render();
+    chart6.render();
+    chart7.render();
+    chart8.render();
 
-  chart9.render();
-  chart10.render();
-  chart11.render();
-  chart12.render();
+    chart9.render();
+    chart10.render();
+    chart11.render();
+  };
+
+  setTimeout(function () {
+    allCharts.forEach((element) => {
+      element.classList.toggle("hidden");
+    });
+
+    titles.forEach((element) => {
+      element.classList.toggle("hidden");
+    });
+
+    //show tony the t-rex searching/loading
+    loading.classList.toggle("hidden");
+  }, 1);
+  searchBtn.addEventListener("click", () => {
+    //show tony the t-rex searching/loading
+
+    loading.classList.toggle("hidden");
+
+    //hide tony when search result is found
+    setTimeout(function () {
+      allCharts.forEach((element) => {
+        element.classList.toggle("hidden");
+      });
+
+      titles.forEach((element) => {
+        element.classList.toggle("hidden");
+      });
+
+      //show tony the t-rex searching/loading
+      loading.classList.toggle("hidden");
+    }, 3000);
+
+    //if not found then show animation for not found
+  });
+  changeTitle();
+  renderCharts();
 };
