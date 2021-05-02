4b1dc2f599f873be2e2f823fe63c218510ae896a
Kevin Nguyen <Kevin.Nguyen0509@gmail.com>
2021-05-01 17:28:37 -0700

    df

--- JavascriptTutorialsRemaid/16-Asynchronous/starter/script.js
+++ JavascriptTutorialsRemaid/16-Asynchronous/starter/script.js
@@ -4,34 +4,53 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+const renderCountry = function (data, className = '') {
+  const html = `
+    <article class="country ${className}">
+    <img class="country__img" src="${data.flag}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${data.population}</p>
+      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+      <p class="country__row"><span>💰</span>${data.currencies[0].code}</p>
+    </div>
+    </article>
+    `;
 
-const getCountryData = country => {
-  const request = new XMLHttpRequest();
-  request.open('GET', `https://restcountries.eu/rest/v2/name/${country}`);
-  request.send();
-
-  request.addEventListener('load', function () {
-    const [data] = JSON.parse(this.responseText);
-    console.log(data);
-
-    const html = `
-<article class="country">
-<img class="country__img" src="${data.flag}" />
-<div class="country__data">
-  <h3 class="country__name">${data.name}</h3>
-  <h4 class="country__region">${data.region}</h4>
-  <p class="country__row"><span>👫</span>${data.population}</p>
-  <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-  <p class="country__row"><span>💰</span>${data.currencies[0].code}</p>
-</div>
-</article>
-`;
-
-    countriesContainer.innerHTML = html;
-    //countriesContainer.insertAdjacentHTML('beforeend', html);
-    countriesContainer.style.opacity = 1;
-  });
+  //countriesContainer.innerHTML = html;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
 };
+// const getCountryAndNeighbor = country => {
+//   const request = new XMLHttpRequest();
+//   request.open('GET', `https://restcountries.eu/rest/v2/name/${country}`);
+//   request.send();
 
-const userFrom = prompt('Hello, where are you from?');
-getCountryData(userFrom);
+//   request.addEventListener('load', function () {
+//     const [data] = JSON.parse(this.responseText);
+//     console.log(data);
+
+//     //render country 1
+//     renderCountry(data);
+//     //render country 2
+//     const [neighbor] = data.borders;
+//     console.log(neighbor);
+
+//     if (!neighbor) return;
+
+//     //AJAX call
+//     const request2 = new XMLHttpRequest();
+//     request2.open('GET', `https://restcountries.eu/rest/v2/alpha/${neighbor}`);
+//     request2.send();
+
+//     request2.addEventListener('load', function () {
+//       const data2 = JSON.parse(this.responseText);
+//       console.log(data2);
+
+//       renderCountry(data2, 'neighbour');
+//     });
+//   });
+// };
+
+// getCountryAndNeighbor('france');
