bfa539c50af8d5e859b27f364fdfd404fbaf144f
Kevin Nguyen <Kevin.Nguyen0509@gmail.com>
2021-05-10 01:11:05 -0700

    removed some comments

--- nodejs/complete-nodepractice/4-natours/after-section-06/routes/tourRoutes.js
+++ nodejs/complete-nodepractice/4-natours/after-section-06/routes/tourRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const tourController = require('./../controllers/tourController');
+
 const router = express.Router();
 
 router.param('id', tourController.checkID); //Checks if the Id is valid and if it's in the database. Id's over the length are the array are not valid
