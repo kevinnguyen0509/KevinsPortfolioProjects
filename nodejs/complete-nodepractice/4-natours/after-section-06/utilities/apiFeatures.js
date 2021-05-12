871de7a90056bd97f5548e33fc4b03adf616b423
Kevin Nguyen <Kevin.Nguyen0509@gmail.com>
2021-05-11 23:14:11 -0700

    corrected export to exports in a file to correct bug

--- nodejs/complete-nodepractice/4-natours/after-section-06/utilities/apiFeatures.js
+++ nodejs/complete-nodepractice/4-natours/after-section-06/utilities/apiFeatures.js
@@ -19,7 +19,7 @@ class APIFeatures {
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
 
     this.query = this.query.find(JSON.parse(queryStr));
-    //console.log(queryStr);
+    console.log(this.queryString);
 
     return this;
   }
@@ -27,7 +27,6 @@ class APIFeatures {
   sort() {
     if (this.queryString.sort) {
       const sortBy = this.queryString.sort.split(',').join(' ');
-
       this.query = this.query.sort(sortBy);
     } else {
       this.query = this.query.sort('-_id');
